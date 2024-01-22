17d8ddcb64aa28943b0b21e0925e9ebfc23e684c
edwardoboh <obohedwarrd@gmail.com>
2024-01-22 14:02:06 +0100

    feat: set up discovery server for moviemicro services

---
+++ src/test/java/com/edwardoboh/moviemicro/movieeurekaserver/MovieEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwardoboh.moviemicro.movieeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
